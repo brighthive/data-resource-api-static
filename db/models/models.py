8648c5bae0cca93092e380559b97d6a3915f13dd
Gregory Mundy <gregmundy@gmail.com>
2019-02-07 11:02:39 -0500

    chore(Db): add schema for provider and associated relationships

--- db/models/models.py
+++ db/models/models.py
@@ -2,14 +2,59 @@ from sqlalchemy.dialects.postgresql.json import JSONB
 from app.app import db
 
 
+class EntityType(db.Model):
+    __tablename__ = 'entity_type'
+    type_id = db.Column(db.Integer, primary_key=True)
+    type_name = db.Column(db.String(100), nullable=False)
+
+
 class Provider(db.Model):
-    __tablename__ = 'providers'
+    __tablename__ = 'provider'
     provider_id = db.Column(db.Integer, primary_key=True)
     provider_name = db.Column(db.String(100), nullable=False)
-    provider_alternate_name = db.Column(db.String(100), nullable=False)
-    # entity type id
-    optional_fields = db.Column(JSONB)
-    user_provided_fields = db.Column(JSONB)
+    provider_alternate_name = db.Column(db.String(100), nullable=True)
+    entity_type_id = db.Column(db.Integer, db.ForeignKey(
+        EntityType.type_id, ondelete='CASCADE'))
+    provider_full_address = db.Column(db.String(2048), nullable=True)
+    provider_description = db.Column(db.String(1024), nullable=True)
+    provider_contact_email = db.Column(db.String(256), nullable=True)
+    provider_url = db.Column(db.String(1024), nullable=True)
+    year_incorporated = db.Column(db.Integer, nullable=True)
+    programs = db.relationship(
+        'Program', backref='provider', lazy=True, passive_deletes=True)
+    locations = db.relationship(
+        'GeographicLocation', backref='provider', lazy=True,
+        passive_deletes=True)
+
+
+class GeographicLocation(db.Model):
+    __tablename__ = 'geographic_location'
+
+    location_id = db.Column(db.Integer, primary_key=True)
+    location_name = db.Column(db.String(100), nullable=False)
+    provider_id = db.Column(db.Integer, db.ForeignKey(
+        Provider.provider_id, ondelete='CASCADE'))
+    location_description = db.Column(db.String(256), nullable=True)
+    transportation = db.Column(db.String(256), nullable=True)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
+    location_full_address = db.Column(db.String(650), nullable=True)
+    location_address = db.relationship(
+        'PhysicalAddress', backref='geographic_location',
+        lazy=True, uselist=False, passive_deletes=True)
+
+
+class PhysicalAddress(db.Model):
+    __tablename__ = 'physical_address'
+
+    address_id = db.Column(db.Integer, primary_key=True)
+    location_id = db.Column(db.Integer, db.ForeignKey(
+        GeographicLocation.location_id, ondelete='CASCADE'))
+    address = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(20), nullable=False)
+    postal_code = db.Column(db.String(20), nullable=False)
+    country = db.Column(db.String(2), nullable=False)
 
 
 class ProgramCategory(db.Model):
@@ -20,7 +65,7 @@ class ProgramCategory(db.Model):
 
 
 class Program(db.Model):
-    __tablename__ = 'programs'
+    __tablename__ = 'program'
 
     # required fields
     program_id = db.Column(db.Integer, primary_key=True)
@@ -48,14 +93,14 @@ class Program(db.Model):
 
 
 class Credential(db.Model):
-    __tablename__ = 'credentials'
+    __tablename__ = 'credential'
     credential_id = db.Column(db.Integer, primary_key=True)
     optional_fields = db.Column(JSONB)
     user_provided_fields = db.Column(JSONB)
 
 
 class Participant(db.Model):
-    __tablename__ = 'participants'
+    __tablename__ = 'participant'
     participant_id = db.Column(db.Integer, primary_key=True)
     optional_fields = db.Column(JSONB)
     user_provided_fields = db.Column(JSONB)
