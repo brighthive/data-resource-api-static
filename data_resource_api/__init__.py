e854f5349266471157c66d8016ba2f5702ca601f
Gregory Mundy <gregmundy@gmail.com>
2019-02-20 16:39:38 -0500

    feat(Auth): add a token database lookup to make the fake auth handler a bit more realistic

--- data_resource_api/__init__.py
+++ data_resource_api/__init__.py
@@ -2,7 +2,7 @@ from data_resource_api.config import Config, ConfigurationFactory
 from data_resource_api.app import app, db
 from data_resource_api.db import Program, Provider, Participant,\
     Credential, CredentialType, ProgramPotentialOutcome, ProgramPrerequisite,\
-    EntityType, GeographicLocation, PhysicalAddress
+    EntityType, GeographicLocation, PhysicalAddress, Token
 from data_resource_api.validator import Validator, ValidatorNotFoundError,\
     SchemaFormatError
 from data_resource_api.utils.utilties import DatabaseConfigurationUtility
--- data_resource_api/auth/fake_auth.py
+++ data_resource_api/auth/fake_auth.py
@@ -5,6 +5,8 @@
 from functools import wraps
 from flask import request
 from flask_restful import abort
+from data_resource_api.app.app import db
+from data_resource_api.db import Token
 
 
 def login_required(f):
@@ -14,7 +16,9 @@ def login_required(f):
             token = str(request.headers['Authorization'])
             token = token.split(' ')
             if str(token[0]).upper() == 'BEARER' and len(token[1]) > 0:
-                pass
+                found_token = Token.query.filter_by(token=token[1]).first()
+                if found_token is None:
+                    abort(401)
             else:
                 abort(401)
         except Exception:
