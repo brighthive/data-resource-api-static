fefc628bb0c32bfbfe8138e8cc94497ce3e822b5
Gregory Mundy <gregmundy@gmail.com>
2019-02-22 15:36:00 -0500

    chore: add separate db population mechanism for integration vs unit testing

--- tests/test_database.py
+++ tests/test_database.py
@@ -12,7 +12,8 @@ from data_resource_api import app, db, Program, Provider, Participant,\
     ProgramPrerequisite, EntityType, GeographicLocation,\
     PhysicalAddress, DatabaseConfigurationUtility, Token
 
-if str(os.getenv('APP_ENV')).upper() == 'INTEGRATION':
+app_environment = str(os.getenv('APP_ENV')).upper()
+if app_environment == 'INTEGRATION':
     database_fixture = DatabaseConfigurationUtility('INTEGRATION', False)
 else:
     database_fixture = DatabaseConfigurationUtility('TEST', False)
@@ -40,7 +41,10 @@ PARTICIPANTS = os.path.join(JSON_FIXTURES, 'participants.json')
 
 @before
 def setup_test_database():
-    database_fixture.start_database()
+    if app_environment != 'INTEGRATION':
+        database_fixture.start_database()
+    else:
+        database_fixture.start_database(True)
     token = Token('269952015c751bcd64428e6e77c355a7')
     db.session.add(token)
     db.session.commit()
@@ -48,192 +52,195 @@ def setup_test_database():
 
 @after
 def teardown_test_database():
-    database_fixture.stop_database()
+    if app_environment != 'INTEGRATION':
+        database_fixture.stop_database()
 
 
 @describe('Test Database Models')
 def _():
-    @it('Should perform CRUD operations on database tables',
-        tags=['essential'])
+    @it('Should perform CRUD operations on database tables')
     def _():
-        # load program prerequisites:
-        with open(PROGRAM_PREREQUISITES, 'r') as f:
-            data = json.load(f)
+        # skip loading the database if in integration mode
+        if app_environment != 'INTEGRATION':
+            # load program prerequisites:
+            with open(PROGRAM_PREREQUISITES, 'r') as f:
+                data = json.load(f)
 
-        for item in data['program_prerequisites']:
-            prereq = ProgramPrerequisite(item['name'], item['id'])
-            db.session.add(prereq)
-        db.session.commit()
-        result = ProgramPrerequisite.query.all()
-        expect(len(result)).to(equal(len(data['program_prerequisites'])))
+            for item in data['program_prerequisites']:
+                prereq = ProgramPrerequisite(item['name'], item['id'])
+                db.session.add(prereq)
+            db.session.commit()
+            result = ProgramPrerequisite.query.all()
+            expect(len(result)).to(equal(len(data['program_prerequisites'])))
 
-        # load program potential outcomes
-        with open(PROGRAM_POTENTIAL_OUTCOMES, 'r') as f:
-            data = json.load(f)
+            # load program potential outcomes
+            with open(PROGRAM_POTENTIAL_OUTCOMES, 'r') as f:
+                data = json.load(f)
 
-        for item in data['program_potential_outcomes']:
-            outcome = ProgramPotentialOutcome(item['name'], item['id'])
-            db.session.add(outcome)
-        db.session.commit()
-        result = ProgramPotentialOutcome.query.all()
-        expect(len(result)).to(equal(len(data['program_potential_outcomes'])))
+            for item in data['program_potential_outcomes']:
+                outcome = ProgramPotentialOutcome(item['name'], item['id'])
+                db.session.add(outcome)
+            db.session.commit()
+            result = ProgramPotentialOutcome.query.all()
+            expect(len(result)).to(
+                equal(len(data['program_potential_outcomes'])))
 
-        # load entity types
-        with open(ENTITY_TYPES, 'r') as f:
-            data = json.load(f)
+            # load entity types
+            with open(ENTITY_TYPES, 'r') as f:
+                data = json.load(f)
 
-        for item in data['entity_types']:
-            entity = EntityType(item['name'], item['id'])
-            db.session.add(entity)
-        db.session.commit()
-        result = EntityType.query.all()
-        expect(len(result)).to(equal(len(data['entity_types'])))
+            for item in data['entity_types']:
+                entity = EntityType(item['name'], item['id'])
+                db.session.add(entity)
+            db.session.commit()
+            result = EntityType.query.all()
+            expect(len(result)).to(equal(len(data['entity_types'])))
 
-        # load credential types
-        with open(CREDENTIAL_TYPES, 'r') as f:
-            data = json.load(f)
+            # load credential types
+            with open(CREDENTIAL_TYPES, 'r') as f:
+                data = json.load(f)
 
-        for item in data['credential_types']:
-            credential_type = CredentialType(
-                item['credential_type'], item['audience_level'],
-                item['credential_type_id'])
-            db.session.add(credential_type)
-        db.session.commit()
-        result = CredentialType.query.all()
-        expect(len(result)).to(equal(len(data['credential_types'])))
+            for item in data['credential_types']:
+                credential_type = CredentialType(
+                    item['credential_type'], item['audience_level'],
+                    item['credential_type_id'])
+                db.session.add(credential_type)
+            db.session.commit()
+            result = CredentialType.query.all()
+            expect(len(result)).to(equal(len(data['credential_types'])))
 
-        # load providers
-        with open(PROVIDERS, 'r') as f:
-            data = json.load(f)
+            # load providers
+            with open(PROVIDERS, 'r') as f:
+                data = json.load(f)
 
-        for item in data['providers']:
-            provider = Provider(
-                item['provider_name'],
-                item['entity_type_id'],
-                item['provider_alternate_name'],
-                item['provider_full_address'],
-                item['provider_description'],
-                item['provider_contact_email'],
-                item['provider_url'],
-                item['year_incorporated']
-            )
-            db.session.add(provider)
-        db.session.commit()
-        result = Provider.query.all()
-        expect(len(result)).to(equal(len(data['providers'])))
+            for item in data['providers']:
+                provider = Provider(
+                    item['provider_name'],
+                    item['entity_type_id'],
+                    item['provider_alternate_name'],
+                    item['provider_full_address'],
+                    item['provider_description'],
+                    item['provider_contact_email'],
+                    item['provider_url'],
+                    item['year_incorporated']
+                )
+                db.session.add(provider)
+            db.session.commit()
+            result = Provider.query.all()
+            expect(len(result)).to(equal(len(data['providers'])))
 
-        # load locations
-        with open(LOCATIONS, 'r') as f:
-            data = json.load(f)
+            # load locations
+            with open(LOCATIONS, 'r') as f:
+                data = json.load(f)
 
-        for item in data['locations']:
-            location = GeographicLocation(
-                item['location_name'],
-                item['provider_id'],
-                item['location_description'],
-                item['transportation'],
-                item['latitude'],
-                item['longitude'],
-                item['location_full_address'],
-                item['location_id']
-            )
-            db.session.add(location)
-        db.session.commit()
-        result = GeographicLocation.query.all()
-        expect(len(result)).to(equal(len(data['locations'])))
+            for item in data['locations']:
+                location = GeographicLocation(
+                    item['location_name'],
+                    item['provider_id'],
+                    item['location_description'],
+                    item['transportation'],
+                    item['latitude'],
+                    item['longitude'],
+                    item['location_full_address'],
+                    item['location_id']
+                )
+                db.session.add(location)
+            db.session.commit()
+            result = GeographicLocation.query.all()
+            expect(len(result)).to(equal(len(data['locations'])))
 
-        # load addresses
-        with open(ADDRESSES, 'r') as f:
-            data = json.load(f)
+            # load addresses
+            with open(ADDRESSES, 'r') as f:
+                data = json.load(f)
 
-        for item in data['addresses']:
-            address = PhysicalAddress(
-                item['location_id'],
-                item['address'],
-                item['city'],
-                item['state'],
-                item['postal_code'],
-                item['country'],
-                item['address_id']
-            )
-            db.session.add(address)
-        db.session.commit()
-        result = PhysicalAddress.query.all()
-        expect(len(result)).to(equal(len(data['addresses'])))
+            for item in data['addresses']:
+                address = PhysicalAddress(
+                    item['location_id'],
+                    item['address'],
+                    item['city'],
+                    item['state'],
+                    item['postal_code'],
+                    item['country'],
+                    item['address_id']
+                )
+                db.session.add(address)
+            db.session.commit()
+            result = PhysicalAddress.query.all()
+            expect(len(result)).to(equal(len(data['addresses'])))
 
-        # load credentials
-        with open(CREDENTIALS, 'r') as f:
-            data = json.load(f)
+            # load credentials
+            with open(CREDENTIALS, 'r') as f:
+                data = json.load(f)
 
-        for item in data['credentials']:
-            credential = Credential(
-                provider_id=item['provider_id'],
-                name=item['credential_name'],
-                description=item['credential_description'],
-                credential_type_id=item['credential_type_id'],
-                credential_status_type=item['credential_status_type'],
-                audience=item['audience'],
-                language=item['language'],
-                ce_ctid=item['ce_ctid'],
-                webpage=item['webpage']
-            )
-            db.session.add(credential)
-        db.session.commit()
-        result = Credential.query.all()
-        expect(len(result)).to(equal(len(data['credentials'])))
-
-        # load programs
-        with open(PROGRAMS, 'r') as f:
-            data = json.load(f)
-            for item in data['programs']:
-                program = Program(
-                    name=item['program_name'],
-                    code=item['program_code'],
-                    description=item['program_description'],
-                    status=item['program_status'],
-                    fees=item['program_fees'],
+            for item in data['credentials']:
+                credential = Credential(
                     provider_id=item['provider_id'],
-                    location_id=item['location_id'],
-                    eligibility_criteria=item['eligibility_criteria'],
-                    potential_outcome_id=item['potential_outcome_id'],
-                    program_url=item['program_url'],
-                    credential_earned=item['credential_earned'],
-                    contact_phone=item['program_contact_phone'],
-                    contact_email=item['program_contact_email'],
-                    languages=item['languages'],
-                    intake=item['current_intake_capacity'],
-                    offering_model=item['program_offering_model'],
-                    length_hours=item['program_length_hours'],
-                    length_weeks=item['program_length_weeks'],
-                    prereq_id=item['prerequisite_id'],
-                    program_soc=item['program_soc'],
-                    funding=item['funding_sources'],
-                    on_etpl=item['on_etpl'],
-                    cost_of_books=item['cost_of_books_and_supplies']
+                    name=item['credential_name'],
+                    description=item['credential_description'],
+                    credential_type_id=item['credential_type_id'],
+                    credential_status_type=item['credential_status_type'],
+                    audience=item['audience'],
+                    language=item['language'],
+                    ce_ctid=item['ce_ctid'],
+                    webpage=item['webpage']
                 )
-                db.session.add(program)
+                db.session.add(credential)
             db.session.commit()
-            result = Program.query.all()
-            expect(len(result)).to(equal(len(data['programs'])))
+            result = Credential.query.all()
+            expect(len(result)).to(equal(len(data['credentials'])))
 
-        # load participants
-        with open(PARTICIPANTS, 'r') as f:
-            data = json.load(f)
-            for item in data['participants']:
-                participant = Participant(
-                    program_id=item['program_id'],
-                    entry_date=item['entry_date'],
-                    exit_date=item['exit_date'],
-                    exit_type=item['exit_type'],
-                    exit_reason=item['exit_reason'],
-                    id=item['participant_id']
-                )
-                db.session.add(participant)
-            db.session.commit()
-            result = Participant.query.all()
-            expect(len(result)).to(equal(len(data['participants'])))
+            # load programs
+            with open(PROGRAMS, 'r') as f:
+                data = json.load(f)
+                for item in data['programs']:
+                    program = Program(
+                        name=item['program_name'],
+                        code=item['program_code'],
+                        description=item['program_description'],
+                        status=item['program_status'],
+                        fees=item['program_fees'],
+                        provider_id=item['provider_id'],
+                        location_id=item['location_id'],
+                        eligibility_criteria=item['eligibility_criteria'],
+                        potential_outcome_id=item['potential_outcome_id'],
+                        program_url=item['program_url'],
+                        credential_earned=item['credential_earned'],
+                        contact_phone=item['program_contact_phone'],
+                        contact_email=item['program_contact_email'],
+                        languages=item['languages'],
+                        intake=item['current_intake_capacity'],
+                        offering_model=item['program_offering_model'],
+                        length_hours=item['program_length_hours'],
+                        length_weeks=item['program_length_weeks'],
+                        prereq_id=item['prerequisite_id'],
+                        program_soc=item['program_soc'],
+                        funding=item['funding_sources'],
+                        on_etpl=item['on_etpl'],
+                        cost_of_books=item['cost_of_books_and_supplies']
+                    )
+                    db.session.add(program)
+                db.session.commit()
+                result = Program.query.all()
+                expect(len(result)).to(equal(len(data['programs'])))
 
-        # find programs provided by a provider
-        provider = Provider.query.first()
-        program = Program.query.first()
-        credential = Credential.query.first()
+            # load participants
+            with open(PARTICIPANTS, 'r') as f:
+                data = json.load(f)
+                for item in data['participants']:
+                    participant = Participant(
+                        program_id=item['program_id'],
+                        entry_date=item['entry_date'],
+                        exit_date=item['exit_date'],
+                        exit_type=item['exit_type'],
+                        exit_reason=item['exit_reason'],
+                        id=item['participant_id']
+                    )
+                    db.session.add(participant)
+                db.session.commit()
+                result = Participant.query.all()
+                expect(len(result)).to(equal(len(data['participants'])))
+
+            # find programs provided by a provider
+            provider = Provider.query.first()
+            program = Program.query.first()
+            credential = Credential.query.first()
